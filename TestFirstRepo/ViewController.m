402353adf2b30e7f666575afa35d44f4fcc2a07a
Tatsuya SETO <tatsuya.seto@recochoku.co.jp>
2015-12-22 14:49:48 +0900

    zzz = 1追加

--- TestFirstRepo/ViewController.m
+++ TestFirstRepo/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // master change
+    int zzz = 1;
 }
 
 - (void)didReceiveMemoryWarning {
